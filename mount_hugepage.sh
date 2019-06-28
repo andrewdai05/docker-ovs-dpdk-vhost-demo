fedf1979026cf1dfd45ce83744f2996337cfa151
xiarui <xiarui0428@163.com>
2019-06-28 10:15:39 -0400

    del

--- mount_hugepage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#sudo mkdir -p /dev/hugepages
-#sudo mount -t hugetlbfs none /dev/hugepages
-sudo mkdir -p /mnt/huge_dpdk
-sudo mount -t hugetlbfs -o pagesize=1G none /mnt/huge_dpdk
-sudo mkdir -p /mnt/huge_pktgen
-sudo mount -t hugetlbfs -o pagesize=1G none /mnt/huge_pktgen
